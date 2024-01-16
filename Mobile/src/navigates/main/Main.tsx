c04c259ec0b3eecfd6a1ae8b154312c68149d9ff
Ho Thi Bich <bich.ho24@student.passerellesnumeriques.org>
2024-01-16 16:03:30 +0700

    style/FAN-8: design choose role

--- Mobile/src/navigates/main/Main.tsx
+++ Mobile/src/navigates/main/Main.tsx
@@ -1,11 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { QueriesObserver, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SelectRole from '../../screens/accounts/SelectRole';
+const queryClient = new QueryClient();
 const Main = () => {
+  const Stack = createStackNavigator();
   return (
-    <View>
-      <Text>Maint</Text>
-    </View>
+    <NavigationContainer>
+      <QueryClientProvider client ={queryClient}>
+        <Stack.Navigator>
+          <Stack.Screen name="SelectRole" component={SelectRole} options={{ headerShown: false }} />
+        </Stack.Navigator>
+
+      </QueryClientProvider>
+    </NavigationContainer>
   )
 }
 
