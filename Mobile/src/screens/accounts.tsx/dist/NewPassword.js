e77a0679adf1d298e490c76eb2aa34d8e9b39150
TranQuocHuuSOS <0tranquochuu02003@gmail.com>
2024-01-22 16:48:37 +0700

    feat(FAN-96) Implement call api reset password

--- Mobile/src/screens/accounts.tsx/dist/NewPassword.js
+++ Mobile/src/screens/accounts.tsx/dist/NewPassword.js
@@ -1,10 +1,79 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 exports.__esModule = true;
 var react_native_1 = require("react-native");
+var native_1 = require("@react-navigation/native");
 var react_1 = require("react");
 var formik_1 = require("formik");
+var useResetNewPassword_1 = require("../../hooks/useResetNewPassword");
 var NewPassword = function () {
-    return (react_1["default"].createElement(formik_1.Formik, { initialValues: { password: '', passwords: '' }, onSubmit: function (values) { return console.log(values); } }, function (_a) {
+    var navigation = native_1.useNavigation();
+    var route = native_1.useRoute();
+    var resetPasswordToken = route.params.resetPasswordToken;
+    console.log(resetPasswordToken);
+    var handleResetSubmit = function (values) { return __awaiter(void 0, void 0, void 0, function () {
+        var newPassword;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    newPassword = {
+                        tokenPassword: resetPasswordToken,
+                        newPassword: values.newpassword,
+                        confirmPassword: values.confirmpassword
+                    };
+                    return [4 /*yield*/, useResetNewPassword_1["default"](newPassword)
+                            .then(function (res) {
+                            if (res.status !== 201) {
+                                react_native_1.Alert.alert(res.message);
+                            }
+                            else {
+                                navigation.navigate("SignIn");
+                            }
+                        })["catch"](function (error) {
+                            console.log(error);
+                        })];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    }); };
+    return (react_1["default"].createElement(formik_1.Formik, { initialValues: { newpassword: '', confirmpassword: '' }, onSubmit: handleResetSubmit }, function (_a) {
         var handleChange = _a.handleChange, handleBlur = _a.handleBlur, handleSubmit = _a.handleSubmit, values = _a.values;
         return (react_1["default"].createElement(react_native_1.KeyboardAvoidingView, { behavior: react_native_1.Platform.OS === 'ios' ? 'padding' : 'height', style: styles.confirmContainer },
             react_1["default"].createElement(react_native_1.ScrollView, { contentContainerStyle: { flexGrow: 1 } },
@@ -15,11 +84,11 @@ var NewPassword = function () {
                         react_1["default"].createElement(react_native_1.View, { style: styles.spaceForm },
                             react_1["default"].createElement(react_native_1.Text, { style: styles.titles }, "M\u00E2\u0323t kh\u00E2\u0309u m\u01A1\u0301i"),
                             react_1["default"].createElement(react_native_1.View, { style: styles.spaceContainer },
-                                react_1["default"].createElement(react_native_1.TextInput, { style: styles.inputCode, onChangeText: handleChange('passwords'), onBlur: handleBlur('passwords'), value: values.passwords })),
+                                react_1["default"].createElement(react_native_1.TextInput, { style: styles.inputCode, onChangeText: handleChange('newpassword'), onBlur: handleBlur('newpassword'), value: values.newpassword })),
                             react_1["default"].createElement(react_native_1.Text, { style: styles.titles }, "Xa\u0301c th\u01B0\u0323c m\u00E2\u0323t kh\u00E2\u0309u m\u01A1\u0301i"),
                             react_1["default"].createElement(react_native_1.View, { style: styles.spaceContainer },
-                                react_1["default"].createElement(react_native_1.TextInput, { style: styles.inputCode, onChangeText: handleChange('password'), onBlur: handleBlur('password'), value: values.password })),
-                            react_1["default"].createElement(react_native_1.TouchableOpacity, { style: styles.buttonConfirm },
+                                react_1["default"].createElement(react_native_1.TextInput, { style: styles.inputCode, onChangeText: handleChange('confirmpassword'), onBlur: handleBlur('confirmpassword'), value: values.confirmpassword })),
+                            react_1["default"].createElement(react_native_1.TouchableOpacity, { style: styles.buttonConfirm, onPress: handleSubmit },
                                 react_1["default"].createElement(react_native_1.Text, { style: styles.textConfirm }, "XA\u0301C TH\u01AF\u0323C"))))),
                 react_1["default"].createElement(react_native_1.View, { style: styles.footer },
                     react_1["default"].createElement(react_native_1.ImageBackground, { source: require('../../assets/ForgotPassword/background.png'), resizeMode: "cover", style: styles.bgImg },
@@ -34,7 +103,7 @@ var styles = react_native_1.StyleSheet.create({
     },
     body: {
         flex: 1,
-        position: "absolute",
+        position: 'absolute',
         zIndex: 100,
         justifyContent: 'center',
         width: '100%'
@@ -58,7 +127,7 @@ var styles = react_native_1.StyleSheet.create({
     },
     titles: {
         fontSize: 22,
-        color: "#394C6D",
+        color: '#394C6D',
         marginHorizontal: 40
     },
     titleCode: {
@@ -71,10 +140,10 @@ var styles = react_native_1.StyleSheet.create({
         borderRadius: 10,
         marginTop: 5,
         borderWidth: 1,
-        width: "80%"
+        width: '80%'
     },
     spaceContainer: {
-        alignItems: "center"
+        alignItems: 'center'
     },
     timeInput: {
         fontSize: 15,
@@ -82,8 +151,8 @@ var styles = react_native_1.StyleSheet.create({
     },
     sentBack: {
         fontSize: 15,
-        fontWeight: "bold",
-        color: "#0000ff"
+        fontWeight: 'bold',
+        color: '#0000ff'
     },
     container: {
         marginTop: 100
@@ -94,8 +163,8 @@ var styles = react_native_1.StyleSheet.create({
         borderRadius: 10,
         height: 60,
         marginHorizontal: 40,
-        alignItems: "center",
-        justifyContent: "center",
+        alignItems: 'center',
+        justifyContent: 'center',
         marginTop: 40
     },
     textConfirm: {
@@ -104,13 +173,13 @@ var styles = react_native_1.StyleSheet.create({
         fontWeight: 'bold'
     },
     demo: {
-        marginTop: "60%",
-        marginRight: "-60%"
+        marginTop: '60%',
+        marginRight: '-60%'
     },
     spaceForm: {
         marginTop: 50
     },
     demoImg: {
-        marginTop: "130%"
+        marginTop: '130%'
     }
 });
